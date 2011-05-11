8510f6b25ee7ce92d97fa84bf1dfbd9ffc3c47a5
shong <sewoom@gmail.com>
2011-05-11 15:03:23 -0700

    Edited config/initializers/mongo_config.rb via GitHub

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,3 +1,3 @@
-#!MongoMapper.connection = Mongo::Connection.new('ec2-204-236-177-224.us-west-1.compute.amazonaws.com', 27017)
+MongoMapper.connection = Mongo::Connection.new('ec2-67-202-30-34.compute-1.amazonaws.com', 27017)
 #!MongoMapper.database = "#{app_name}_#{node[:environment][:framework_env]}"
-#!MongoMapper.database.authenticate("root", "#{user[:password]}")
+MongoMapper.database.authenticate("deploy", "#{user[:password]}")
