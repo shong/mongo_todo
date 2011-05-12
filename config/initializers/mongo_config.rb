919d6574dd59d2dd065912272b20f7aabaaf8863
shong <sewoom@gmail.com>
2011-05-12 16:13:52 -0700

    Edited config/initializers/mongo_config.rb via GitHub

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,1 +1,1 @@
-MongoMapper.connection = Mongo::Connection.new('ec2-205-236-212-236.compute-1.amazonaws.com')
+MongoMapper.connection = Mongo::Connection.new('ec2-184-72-170-2.compute-1.amazonaws.com')
