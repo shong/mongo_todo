476e17e435e7b197636ad0a9e1aa02445bb5e374
shong <sewoom@gmail.com>
2011-05-12 16:49:33 -0700

    Edited config/initializers/mongo_config.rb via GitHub

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,1 +1,3 @@
 MongoMapper.connection = Mongo::Connection.new('10.96.126.5')
+MongoMapper.database = "#{app_name}_#{node[:environment][:framework_env]}"
+MongoMapper.database.authenticate("deploy", "#{user[:password]}")
